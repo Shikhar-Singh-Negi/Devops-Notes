ee5a30335cf11879c756a265724ddb10292a1634
Shikhar <s.snegi1404@gmail.com>
2026-06-28 15:44:34 +0530

    Secure State Management

--- Terraform/Ec2/terraform.tf
+++ Terraform/Ec2/terraform.tf
@@ -6,4 +6,19 @@ terraform {
             version = "~> 5.91.0"
         }
     }
+
+    backend "s3" {
+      bucket = "bucket-using-terraform"
+      key = "terraform.tfstate"
+      region = "us-east-2"
+      dynamodb_table = "State_Table"   #For StateLocking
+
+    }
 }
+
+##note :-
+        # command after changig terraform block
+            # terraform init
+
+# Ab humse agar state bhi delete ho jayegi tha bhi koi dikkat nahi hai
+#  ab state file remote backend backup me save ho rhi hai.
